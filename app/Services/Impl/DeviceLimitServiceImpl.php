4adb405b8bdf728b90ea4f59e4d064152ac8af2c
Dony Yuli Handoko <officialdonyyulihandoko@gmail.com>
2025-10-01 08:52:20 +0700

    commit setup feature/device-limiter2

---
+++ app/Services/Impl/DeviceLimitServiceImpl.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Impl;
+
+class DeviceLimitServiceImpl
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\DeviceLimitServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\SubscribeProvider::class,
     App\Providers\SubscribeServiceProvider::class,
