a30bc2c0e837e031a8eb56932fad1fb177bd9639
Dony Yuli Handoko <officialdonyyulihandoko@gmail.com>
2025-10-19 12:48:46 +0700

    commit final feature/membership-checker

---
+++ app/Console/Commands/CheckMembership.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\CheckMembershipStatus;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Bus\Batch;
+use Illuminate\Support\Facades\Log;
+
+class CheckMembership extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'memberships:check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check and deactivate expired memberships';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Bus::batch([
+            new CheckMembershipStatus(),
+        ])->then(function (Batch $batch) {
+            Log::info('Membership checks completed');
+        })->catch(function (Batch $batch, $e) {
+            Log::error('Membership check failed: ' . $e->getMessage());
+        })->finally(function (Batch $batch) {
+            Log::info('Membership check finished');
+        })->dispatch();
+        // CheckMembershipStatus::dispatch();
+        // $this->info('Membership check job has been dispatched');
+    }
+}
---
+++ app/Services/MembershipService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services;
+
+interface MembershipService
+{
+    public function membershipStatusCheck();
+}
